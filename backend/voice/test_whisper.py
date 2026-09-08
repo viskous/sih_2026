fd2ac80d9ad93ce553375c4e56c89616a9bab719
viskous <151204vishnu@gmail.com>
2026-09-08 18:50:18 +0530

    samples

--- backend/voice/test_whisper.py
+++ backend/voice/test_whisper.py
@@ -2,7 +2,7 @@ import whisper
 
 model = whisper.load_model("small")
 
-result = model.transcribe("voice/sample_audio.mp3", language="hi", task="translate")
+result = model.transcribe("voice/sample_2.mp3", language="hi", task="translate")
 
 print("Translated text (English):")
 print(result["text"])
